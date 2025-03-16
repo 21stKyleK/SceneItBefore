a922beb7ea0991c12788177a7a9f53a42c0e9aec
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2025-03-16 10:40:58 -0500

    Updated MovingProp
    Added rotate and scaling functionality to MovingProp script, using toolmodes 3, 4, 5, and 6. Still didn't fix touch screen issues. New zip file has all of the assets I've been using.

--- Editor/MovingProp.cs
+++ Editor/MovingProp.cs
@@ -5,10 +5,10 @@ public class MovingProp : MonoBehaviour
     public GameObject clickProp = null;
 
     //to determine how the prop should move.
-    //1 = XY movement, 2 = XZ movement
+    //1 = XY movement, 2 = XZ movement, 3 = XY rotation, 4 = XZ rotation, 5 = XY scale, 6 = XZ scale, 7 = delete
     public int toolMode = 0;
-    //prevents the prop from flying off the screen
-    public float limitFactor = 0.01f;
+    //former prevents the prop from flying off the screen, while hte latter is to undo some of the former to make rotates faster
+    public float limitFactor = 0.01f, rotateFactor = 3.8f;
 
     private bool firstMove = true;
 
@@ -38,6 +38,14 @@ public class MovingProp : MonoBehaviour
                 case 2:
                     moveXZ(difference);
                     break;
+                    case 3:
+                        rotateXY(difference); break;
+                    case 4:
+                        rotateXZ(difference); break;
+                    case 5:
+                        scaleXY(difference); break;
+                    case 6:
+                        scaleXZ(difference); break;
                 default:
                     moveXY(difference);
                     break;
@@ -53,11 +61,13 @@ public class MovingProp : MonoBehaviour
         }
     }
 
+    //calculates the difference between the previous cursor postition and the current
     private Vector3 mousePosDiff()
     {
         return (newMousePos - oldMousePos) * limitFactor;
     }
 
+    //move prop possition
     void moveXY(Vector3 difference)
     {
         //set the position of the selected object to itself plus the difference of the mouse position in terms of x and y
@@ -70,5 +80,36 @@ public class MovingProp : MonoBehaviour
         //set the position of the selected object to itself plus the difference of the mouse position in terms of x and z
         clickProp.transform.position = new Vector3(clickProp.transform.position.x + difference.x,
             clickProp.transform.position.y, clickProp.transform.position.z + difference.y);
+    }
+
+    //move prop rotation. Y movement will roate along X axis. Undo the limit factor to speed up rotation
+    void rotateXY(Vector3 difference)
+    {
+        //set the rotation of the selected object to itself plus the difference of the mouse position in terms of x and y
+        clickProp.transform.Rotate(difference.y / (limitFactor * rotateFactor), difference.x / (limitFactor * rotateFactor),
+            0, Space.World);
+    }
+
+    void rotateXZ(Vector3 difference)
+    {
+        //set the rotation of the selected object to itself plus the difference of the mouse position in terms of x and z
+        clickProp.transform.Rotate(difference.y / (limitFactor * rotateFactor), 0,
+            difference.x / (limitFactor * rotateFactor), Space.World);
+        //I think the extra Space.World parameter makes the rotation better
+    }
+
+    //change prop's scale
+    void scaleXY(Vector3 difference)
+    {
+        //set the scale of the selected object to itself plus the difference of the mouse position in terms of x and y
+        clickProp.transform.localScale = new Vector3(clickProp.transform.localScale.x + difference.x,
+            clickProp.transform.localScale.y + difference.y, clickProp.transform.localScale.z);
+    }
+
+    void scaleXZ(Vector3 difference)
+    {
+        //set the scale of the selected object to itself plus the difference of the mouse position in terms of x and z
+        clickProp.transform.localScale = new Vector3(clickProp.transform.localScale.x + difference.x,
+            clickProp.transform.localScale.y, clickProp.transform.localScale.z + difference.y);
     }
 }
